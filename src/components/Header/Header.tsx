97f1a5ee672928ca265204f6f67e703549f07daa
Alba Cardona <alba.cardonajimenez@gmail.com>
2025-03-10 00:11:14 +0100

    re-style homepage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
 import type { Card } from '@/types/types';
 import { useRandomCards } from '@/hooks/useRandomCards';
 import './Header.scss';
-import { Container } from '../ui/Container/Container';
 import { CardItem } from '../CardItem/CardItem';
 
 interface Props {
@@ -14,20 +13,20 @@ export const Header = (props: Props) => {
   const { selectedCards } = useRandomCards({ cards, quantity: 3 });
 
   return (
-    <Container>
+    <>
       <div className="header-container">
         <div className="header-info">
           <h2>The magic cards collection</h2>
           <p>Check the catalogue, save your favourite cards and create your own collections.</p>
         </div>
         <div className="random-cards">
-          {selectedCards.length < 0
-            ? selectedCards?.map((card) => (
-                <CardItem key={card.id} card={card} showCardName={false} />
-              ))
-            : new Array(3).map((item) => <div className="empty-card" key={item} />)}
+          {selectedCards.length > 0 &&
+            selectedCards.map((card) => (
+              <CardItem key={card.id} card={card} showCardName={false} />
+            ))}
         </div>
       </div>
-    </Container>
+      <hr />
+    </>
   );
 };
